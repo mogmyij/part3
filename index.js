3cf0bda481302d83a074d4fa31936e32d26f608c
mogmyij <foonglucas@gmail.com>
2022-10-08 13:13:04 +0800

    part 3

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let phoneBook = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(phoneBook);
+});
+
+app.get("/info", (request, response) => {
+  var size = phoneBook.length;
+  var dateTime = new Date();
+
+  response.send(
+  );
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
