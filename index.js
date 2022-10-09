429c606742339daf65d7d02782edb59061a24535
mogmyij <foonglucas@gmail.com>
2022-10-09 10:19:37 +0800

    part 3.6

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -29,12 +30,57 @@ app.get("/api/persons", (request, response) => {
   response.json(phoneBook);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const personId = parseInt(request.params.id);
+  var person = phoneBook.find((person) => person.id === personId);
+  if (person === undefined) {
+    return response.status(404).json({
+      error: "contact not found",
+    });
+  }
+  response.json(person);
+});
+
 app.get("/info", (request, response) => {
   var size = phoneBook.length;
   var dateTime = new Date();
 
-  response.send(
+  response.write(`<p>There are ${size} contacts saved<p>`);
+  response.write(`${dateTime.toString()}`);
+  response.send();
+});
+
+app.post("/api/persons", (request, response) => {
+  var newPerson = request.body;
+  if (newPerson.name === undefined || newPerson.number === undefined) {
+    return response.status(422).json({
+      error: "invalid contact information",
+    });
+  }
+  var duplicateName = phoneBook.find((person) => person.name == newPerson.name);
+  if (duplicateName !== undefined ) {
+    return response.status(409).json({
+      error: "contact already exists",
+    });
+  }
+
+  newPerson = { id: Math.floor(Math.random() * 10000), ...newPerson };
+  phoneBook.push(newPerson);
+  response.json(newPerson);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const personId = parseInt(request.params.id);
+  var indexOfPersonToDelete = phoneBook.findIndex(
+    (person) => person.id === personId
   );
+  if (indexOfPersonToDelete === -1) {
+    return response.status(404).json({
+      error: "contact not found",
+    });
+  }
+  phoneBook.splice(indexOfPersonToDelete, 1);
+  response.status(204).end();
 });
 
 const PORT = 3001;
